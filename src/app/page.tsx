eac7ac332e0c78a9bbff3219b4dbbe14c0bd90ec
Himanshu2-8 <pragyan.himanshu2004@gmail.com>
2025-12-17 21:16:01 +0530

    fix: bug in useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,36 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { nanoid } from "nanoid"
+import { Button } from "@/components/ui/button";
+import { nanoid } from "nanoid";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-const ANIMALS=["Wolf","Falcon","Shark","Hawk"];
+const ANIMALS = ["Wolf", "Falcon", "Shark", "Hawk"];
 
-export default function Page(){
+export default function Page() {
+  const [id, setId] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const router = useRouter();
 
-    const [id,setId]=useState<string>("");
-    const [username,setUsername]=useState<string>("");
-    const router=useRouter();
+  useEffect(() => {
+    const generateRandomId = () => {
+      const id = nanoid(6);
+      const random = Math.floor(Math.random() * ANIMALS.length);
+      const animal = ANIMALS[random];
+      setId(id);
+      setUsername(animal + id);
+    };
+    generateRandomId();
+  }, []);
 
-    const generateRandomId=()=>{
-        const id=nanoid(6);
-        const random=Math.floor(Math.random()*ANIMALS.length);
-        const animal=ANIMALS[random];
-        setId(id);
-        setUsername(animal+id);
-    }
-
-    useEffect(()=>{
-        generateRandomId();
-    },[]);
-
-    return <div>
-        <h3>{">"}Welcome {username}</h3>
-        <Button onClick={()=>router.push(`/room/${id}`)}>Create Secure Room</Button>
+  return (
+    <div>
+      <h3>
+        {">"}Welcome {username}
+      </h3>
+      <Button onClick={() => router.push(`/room/${id}`)}>
+        Create Secure Room
+      </Button>
     </div>
+  );
 }
